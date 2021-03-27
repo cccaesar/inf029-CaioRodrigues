c2cac35cae064559cc389f1be1d109477aba2a8e
cccaesar <ccsrrdrgscosta@gmail.com>
2021-03-27 17:58:32 -0300

    Adicionado o menu para professores

--- Atividades/Escola/v02/escola.c
+++ Atividades/Escola/v02/escola.c
@@ -1,79 +1,54 @@
 #include <stdio.h>
+#include "pessoaFuncoes.h"
 #define TAM 5
 #define TAM_NOME 20
 #define TAM_CPF 12
+#define MAX_DISCIPLINAS 2
 
 typedef struct{
-	int ano;
-	int mes;
-	int dia;
-} dma;
-
-typedef struct{
-	
+	char nome[TAM_NOME];
+	int semestre;
+	int codigo;
+	int Matricula_professor;
+	pessoa_dados alunos[TAM];
 } disciplina;
 
-typedef struct {
-	int matricula;
-	char nome[TAM_NOME];
-	char sexo;
-	char cpf[12];
-	dma data_nascimento;
-} pessoa_dados;
-
-void mostrarAluno(pessoa_dados aluno);
-void inserirAluno( int qtd_alunos, pessoa_dados lista[] );
+int menu_alunos( pessoa_dados lista_alunos[], int qtd_alunos );
 void removerEnter( char palavra[], int tam );
-
-void mostrarAluno(pessoa_dados aluno){
-	printf("-----\n");
-	printf("Matricula: %d\n", aluno.matricula);
-	printf("Nome do aluno: %s\n", aluno.nome);
-	printf("%d/%d/%d\n", aluno.data_nascimento.ano, aluno.data_nascimento.mes, aluno.data_nascimento.dia);
-	switch(aluno.sexo){
-		case 'M':
-		case 'm':{
-			printf("Sexo: Masculino\n");
-			break;
-		}
-		case 'F':
-		case 'f':{
-			printf("Sexo: Feminino\n");
-			break;
-		}
-		default:{
-			printf("Sexo: Outro\n");
-			break;
-		}
-	}
-	printf("CPF do aluno: %s\n", aluno.cpf);
-	printf("-----\n");
-}
-
-void inserirAluno( int qtd_alunos, pessoa_dados lista[] ){
-	pessoa_dados aluno;
-	printf("Insira o numero de matricula do aluno:\n");
-	scanf("%d",&aluno.matricula);
-	getchar();
-	printf("Insira o nome do aluno:\n");
-	fgets(aluno.nome, TAM_NOME, stdin);
-	removerEnter(aluno.nome , TAM_NOME);
-	printf("Informe o sexo do aluno:\n");
-	aluno.sexo = getchar();
-	getchar();
-	printf("Insira os numeros do CPF do aluno:\n");
-	fgets(aluno.cpf, TAM_CPF, stdin);
-	removerEnter(aluno.cpf , TAM_CPF);
-	printf("Insira ano, mes e dia de nascimento do aluno, respectivamente\n");
-	scanf("%d%d%d",&aluno.data_nascimento.ano,&aluno.data_nascimento.mes,&aluno.data_nascimento.dia);
-	lista[qtd_alunos++] = aluno;
-}
+int menu_prof( pessoa_dados lista_prof[], int qtd_prof );
 
 int main()
 {
 	
-	pessoa_dados aluno, lista_alunos[TAM];
-	int opcao = 1, i, qtd_alunos = 0;
+	pessoa_dados lista_professores[TAM], lista_alunos[TAM];
+	disciplina lista_disciplinas[MAX_DISCIPLINAS];
+	int opcao = 1, i, qtd_alunos = 0, qtd_professores = 0;
+	
+	while(opcao){
+		printf("Digite sua opção:\n");
+		printf("0 - Sair\n");
+		printf("1 - Gerenciar Alunos\n");
+		printf("2 - Gerenciar Professores\n");
+		printf("3 - Gerenciar Disciplinas\n");
+		scanf("%d",&opcao);
+		switch(opcao){
+			case 1:{
+				printf("Abrindo o menu de alunos\n");
+				qtd_alunos = menu_alunos( lista_alunos, qtd_alunos );
+				break;
+			}
+			case 2:{
+				printf("Abrindo o menu de professores\n");
+				qtd_professores = menu_alunos( lista_professores, qtd_professores );
+				break;
+			}
+		}
+	}
+}
+
+int menu_alunos( pessoa_dados lista_alunos[], int qtd_alunos )
+{
+	int opcao = 1, i;
 	
 	while(opcao){
 		printf("Digite sua opção:\n");
@@ -84,25 +59,46 @@ int main()
 		switch(opcao){
 			case 1:{
 				for(i=0; i< qtd_alunos; i++){
-					mostrarAluno(lista_alunos[i]);
+					mostrarPerfil(lista_alunos[i]);
 				}
 				break;
 			}
 			case 2:{
 				
-				inserirAluno( qtd_alunos, lista_alunos);
+				inserirConta( lista_alunos , qtd_alunos, 0 );
 				qtd_alunos++;
 				break;
 			}
 		}
 	}
+	return qtd_alunos;
 }
 
-void removerEnter( char palavra[], int tam ){
-	int i;
-	for(i = 0; i < tam; i++){
-		if( palavra[i] == '\n'){
-			palavra[i] = '\0';
+int menu_prof( pessoa_dados lista_prof[], int qtd_prof )
+{
+	int opcao = 1, i;
+	
+	while(opcao){
+		printf("Digite sua opção:\n");
+		printf("0 - Sair\n");
+		printf("1 - Listar professores\n");
+		printf("2 - Inserir professores\n");
+		scanf("%d",&opcao);
+		switch(opcao){
+			case 1:{
+				for(i=0; i< qtd_prof; i++){
+					mostrarPerfil(lista_prof[i]);
+				}
+				break;
+			}
+			case 2:{
+				
+				inserirConta( lista_prof , qtd_prof, 1 );
+				qtd_prof++;
+				break;
+			}
 		}
 	}
+	return qtd_prof;
 }
+
